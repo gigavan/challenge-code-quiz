dbe6b0e491558f3f8ee7fb6ca1987494521efcc4
Emmanuel <Gigavan@gmail.com>
2022-07-16 22:34:52 -0400

    added more variables for buttons and created some divs

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,3 +4,21 @@ var scoreCounter = 0;
 
 var timeLeft = 75;
 var timeInterval;
+
+var scores = [];
+var timerEl = document.getElementById('timer');
+var quizContainerEl = document.createElement("div");
+var questionEl = document.createElement("h1");
+questionEl.className = "question";
+var answerListEl = document.createElement("ul");
+answerListEl.className = "answer-list";
+var answerChoiceOne = document.createElement("button");
+answerChoiceOne.className = ("btn");
+var answerChoiceTwo = document.createElement("button");
+answerChoiceTwo.className = ("btn");
+var answerChoiceThree = document.createElement("button");
+answerChoiceThree.className = ("btn");
+var answerChoiceFour = document.createElement("button");
+answerChoiceFour.className = ("btn");
+var closingContainerEl = document.createElement("div");
+var viewScoreContainer = document.createElement("div");
